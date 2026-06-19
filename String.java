fa02dbed2b4debeafe94294281d9fb565a8652d4
ASHVITHAb <ashvithameena@gmail.com>
2026-06-19 22:39:11 +0530

    Add Test class to demonstrate string concatenation

---
+++ String.java
@@ -0,0 +1,11 @@
+//String
+public class Test {
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        str = str + " Programming";
+
+        System.out.println(str);
+    }
+}
