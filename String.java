e9ed36e34941b5ebd060354bafa5447b027f3862
ASHVITHAb <ashvithameena@gmail.com>
2026-06-20 22:10:16 +0530

    Add Test class to demonstrate StringBuilder usage

--- String.java
+++ String.java
@@ -9,3 +9,14 @@ public class Test {
         System.out.println(str);
     }
 }
+//StringBuilder
+public class Test {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Programming");
+
+        System.out.println(sb);
+    }
+}
