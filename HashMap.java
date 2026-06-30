c1a65406c82e2b6e118ab3af32942a0a78afb2b2
ASHVITHAb <ashvithameena@gmail.com>
2026-06-30 21:54:18 +0530

    Add Test class with HashMap example

---
+++ HashMap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(101, "Ash");
+        map.put(102, "Ram");
+        map.put(103, "John");
+
+        System.out.println(map);
+    }
+}
