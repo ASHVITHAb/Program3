521f1244edcdea350d09567f5017eeab28301a09
ASHVITHAb <ashvithameena@gmail.com>
2026-07-18 22:39:00 +0530

    Add immutable Employee class with demo

--- Immutable.java
+++ Immutable.java
@@ -1,1 +1,31 @@
+// Immutable Class
+//final class
+final class Employee {
 
+    private final int id;
+    private final String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class ImmutableDemo {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee(101, "Ash");
+
+        System.out.println(emp.getId());
+        System.out.println(emp.getName());
+    }
+}
