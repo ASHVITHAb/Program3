152837ece4b600bd51b77a9a5593d15c78f13c9e
ASHVITHAb <ashvithameena@gmail.com>
2026-07-19 21:31:26 +0530

    Update Collection_framework.java

--- Collection_framework.java
+++ Collection_framework.java
@@ -1,1 +1,15 @@
+//List
+import java.util.ArrayList;
 
+public class ListExample {
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("Java");
+
+        System.out.println(list);
+    }
+}
