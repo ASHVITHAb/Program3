193fa5cddf02b0d645b4b1b3c4da659c553985a6
ASHVITHAb <ashvithameena@gmail.com>
2026-07-20 23:52:32 +0530

    Add HashSet example in Collection_framework.java
    Added a HashSet example to demonstrate set functionality in Java.

--- Collection_framework.java
+++ Collection_framework.java
@@ -18,3 +18,20 @@ public class ListExample {
 //[Java, Python, Java]
 
 //set
+import java.util.HashSet;
+
+public class SetExample {
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<>();
+
+        set.add("Java");
+        set.add("Python");
+        set.add("Java");
+
+        System.out.println(set);
+    }
+}
+
+Output 
+    
